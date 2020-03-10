793dbbe2884478ff85c8ec47d22edcd91f8d4f7e
Ksenia Loseva <loseva@banki.ru>
2020-03-10 13:07:15 +0300

    moved project to root folder of the repo

---
+++ storybook-static/main.4860b9b239f4ee537b2b.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{584:function(n,o,p){p(585),p(747),n.exports=p(1255)},747:function(n,o,p){"use strict";p.r(o);p(748),p(945),p(948)}},[[584,1,2]]]);
---
+++ storybook-static/precache-manifest.f03a2ea8817b05483573a54f573f0398.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f3f27a15b0fc58e17e8be53c060e3b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8b0df00541f8ddf1f904.bundle.js"
+  },
+  {
+    "revision": "55ab4dda347b27394112",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.8b0df00541f8ddf1f904.bundle.js"
+  },
+  {
+    "url": "vendors~main.8b0df00541f8ddf1f904.bundle.js"
+  }
+]);
